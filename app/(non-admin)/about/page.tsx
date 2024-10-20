005c50b265ac177643cd42da8d8a03ff865252cf
manishdotasm <114844168+manishdotasm@users.noreply.github.com>
2024-10-20 18:52:38 +0545

    h

--- app/(non-admin)/about/page.tsx
+++ app/(non-admin)/about/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { MacbookScroll } from "@/components/ui/macbook-scroll";
+import Link from "next/link";
+import { Github } from "lucide-react";
 
 const About = () => {
-	return <div>About</div>;
+	return (
+		<div className="overflow-hidden dark:bg-[#0B0B0F] bg-white w-full">
+			<MacbookScroll
+				title={<span>BLOGS!</span>}
+				badge={
+					<Link href="https://github.com/manishdotasm/blog-application">
+						<Github className="h-10 w-10 transform -rotate-12" />
+					</Link>
+				}
+				src={`/macbook.png`}
+				showGradient={false}
+			/>
+		</div>
+	);
 };
 
 export default About;
