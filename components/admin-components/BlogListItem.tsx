ebdf21ed466c8111ed3ef6166068f03bae55bf58
manishdotasm <114844168+manishdotasm@users.noreply.github.com>
2024-10-20 18:22:08 +0545

    h

--- components/admin-components/BlogListItem.tsx
+++ components/admin-components/BlogListItem.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Button } from "../ui/button";
 import { Delete } from "lucide-react";
 
-const BlogListItem = ({ title, date, mongoID, deleteBlog }) => {
+const BlogListItem = ({ title, date, mongoID, deleteBlog }: { title: any; date: any; mongoID: any; deleteBlog: any }) => {
 	return (
 		<tr className="border-b border-gray-200 dark:border-gray-700">
 			<th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
