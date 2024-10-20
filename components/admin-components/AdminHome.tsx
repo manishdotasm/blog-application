1150725a0f05cc4efa25f36b19dfcc32403eb793
manishdotasm <114844168+manishdotasm@users.noreply.github.com>
2024-10-20 18:37:22 +0545

    h

--- components/admin-components/AdminHome.tsx
+++ components/admin-components/AdminHome.tsx
@@ -395,7 +395,7 @@ export function AdminHome() {
 	];
 
 	return (
-		<div className="w-full h-full -mb-9">
+		<div className="w-full h-full -mb-9 ">
 			<h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center  relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
 				<Cover>ADMIN</Cover> Panel
 			</h1>
