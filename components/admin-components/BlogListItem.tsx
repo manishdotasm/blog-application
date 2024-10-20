d75af2e88219829f21463598f77b128b195cbf8f
manishdotasm <114844168+manishdotasm@users.noreply.github.com>
2024-10-20 18:30:26 +0545

    h

--- components/admin-components/BlogListItem.tsx
+++ components/admin-components/BlogListItem.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import { Button } from "../ui/button";
 import { Delete } from "lucide-react";
 
-const BlogListItem = ({ title, date, mongoID, deleteBlog }: { title: any; date: any; mongoID: any; deleteBlog: any }) => {
+interface BlogListItemProps {
+	title: string;
+	date: string;
+	mongoID: string;
+	deleteBlog: (id: string) => void; // Correctly typing deleteBlog as a function
+}
+
+const BlogListItem: React.FC<BlogListItemProps> = ({ title, date, mongoID, deleteBlog }) => {
 	return (
 		<tr className="border-b border-gray-200 dark:border-gray-700">
 			<th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-				{title ? title : "unknown"}
+				{title || "unknown"}
 			</th>
-			<td className="px-6 py-4">{date ? date : "unknown"}</td>
+			<td className="px-6 py-4">{date || "unknown"}</td>
 			<td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
 				<Button onClick={() => deleteBlog(mongoID)}>
 					<Delete />
